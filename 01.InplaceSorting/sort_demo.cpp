79f7e2b8cc1244adb06a22602b9af2d3332f8f03
Orel-VO <st140793@student.spbu.ru>
2026-06-04 17:23:26 +0300

    Update sort_demo.cpp

--- 01.InplaceSorting/sort_demo.cpp
+++ 01.InplaceSorting/sort_demo.cpp
@@ -11,19 +11,32 @@
 
 using CollInt = CollectingValue<int>;
 
-// Random data
-
+// Random data generation
 std::vector<CollInt> generate_data(size_t n)
 {
     std::vector<CollInt> data(n);
+    std::mt19937 gen(42U);
+    for (size_t i = 0; i < n; ++i)
+    {
+        data[i] = CollInt(static_cast<int>(gen() & 0x7fffffff));
+    }
+    return data;
+}
+
+void shuffle_data(std::vector<CollInt>& data)
+{
+    std::mt19937 gen(42U);
+    std::shuffle(data.begin(), data.end(), gen);
+}
 
 int main()
 {
-    std::vector<size_t> sizes = {100, 500, 1000, 2000};
     std::vector<size_t> sizes = {0, 1, 10, 100, 500, 1000, 2000};
 
     std::cout << "N\tAlgo\t\tComps\t\tSwaps\t\tMoves\n";
     std::cout << "----------------------------------------------------------------\n";
+
+    for (size_t n : sizes)
     {
         auto number_data = generate_data(n);
 
@@ -35,21 +48,21 @@ int main()
         std::cout << n << "\tstd::sort\t" << CollInt::comps << "\t\t" << CollInt::swaps << "\t\t" << CollInt::moves << "\n";
 
         // --- Bubble Sort ---
-
         shuffle_data(number_data);
         CollInt::reset_stats();
         bubble_sort(number_data.begin(), number_data.end());
         assert(std::is_sorted(number_data.begin(), number_data.end()));
         std::cout << n << "\tBubble\t\t" << CollInt::comps << "\t\t" << CollInt::swaps << "\t\t" << CollInt::moves << "\n";
 
-        // --- Insertion Sort ---
-
+        // --- Quick Sort ---
         shuffle_data(number_data);
         CollInt::reset_stats();
         quick_sort(number_data.begin(), number_data.end());
-        std::cout << n << "\tInsertion\t" << CollInt::comps << "\t\t" << CollInt::swaps << "\t\t" << CollInt::moves << "\n";
         assert(std::is_sorted(number_data.begin(), number_data.end()));
         std::cout << n << "\tQuick\t\t" << CollInt::comps << "\t\t" << CollInt::swaps << "\t\t" << CollInt::moves << "\n";
 
         std::cout << "--------------------------------------------------------------\n";
     }
+
+    return 0;
+}
