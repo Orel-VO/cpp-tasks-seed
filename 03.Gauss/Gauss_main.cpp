356ca9c9b2519033786f59d2bef2f737c109a99d
Orel-VO <st140793@student.spbu.ru>
2026-06-04 22:39:26 +0300

    Update Gauss_main.cpp

--- 03.Gauss/Gauss_main.cpp
+++ 03.Gauss/Gauss_main.cpp
@@ -1,31 +1,28 @@
+#include <exception>
 #include <iostream>
-#include <string>
 
-#include <Eigen/Dense>
-#include <lazycsv.hpp>
-
+#include "Gauss_solve.h"
 #include "util.h"
 
 int main(int argc, const char *argv[])
 {
-    auto A = load_csv_to_matrix(argv[1]);
+    if (argc != 2)
+    {
+        std::cerr << "Usage: gauss <input.csv>\n";
+        return 1;
+    }
 
-    Eigen::MatrixXd B(3, 2); // ColMajor по-умолчанию
-    B << 7, 8,
-    9, 10,
-    11, 12;
-
-    Eigen::MatrixXd C = A * B;
-
-    std::cout << "Матрица A:\n" << A << "\n\n";
-    std::cout << "Матрица B:\n" << B << "\n\n";
-    std::cout << "Результат умножения (C = A * B):\n" << C << "\n";
-
-    // Редактирование на месте
-    double c = 2.0;
-    A.row(0) += c * A.row(1);
-    A.coeffRef(1, 1) -= B.coeff(1, 1);
-    std::cout << "Новая матрица A:\n" << A << "\n\n";
+    try
+    {
+        auto ab = load_csv_to_matrix(argv[1]);
+        const auto x = Gauss_solve(ab);
+        print_vector_as_csv(std::cout, x);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
 
     return 0;
 }
