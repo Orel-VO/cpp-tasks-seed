3120d387497d9052503aa33517fbedaaeacf1024
Orel-VO <st140793@student.spbu.ru>
2026-06-04 20:28:36 +0300

    Update base85ed.h

--- 02.Base85/base85ed.h
+++ 02.Base85/base85ed.h
@@ -5,17 +5,21 @@
 #include <cstdint>
 #include <stdexcept>
 
+
 namespace base85
 {
+
 
 class Base85Exception : public std::runtime_error {
 public:
     explicit Base85Exception(const std::string& msg) : std::runtime_error(msg) {}
 };
 
+
 std::vector<uint8_t> encode(std::vector<uint8_t> const &bytes);
+
+
 std::vector<uint8_t> decode(std::vector<uint8_t> const &b85str);
 
-} // namespace base85
 
-#endif // __BASE85ED__
+#endif 
