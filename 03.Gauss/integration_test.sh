89271df1314945f2d1fa22d4f11c2a5c698b6a67
Orel-VO <st140793@student.spbu.ru>
2026-06-04 22:43:23 +0300

    Update integration_test.sh

--- 03.Gauss/integration_test.sh
+++ 03.Gauss/integration_test.sh
@@ -4,4 +4,15 @@ status=true
 
 # ---------------------------------------------------
 
+./gauss AB.csv >X.csv.out
+
+if cmp -s X.csv X.csv.out; then
+  echo Gauss ok!
+else
+  echo Gauss failed! >&2
+  status=false
+fi
+
+# ---------------------------------------------------
+
 $status
